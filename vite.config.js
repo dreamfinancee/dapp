0392771ca81c4e08215c4bcd8c02b5a26e162e63
dreamfinancee <144824997+dreamfinancee@users.noreply.github.com>
2024-09-20 20:41:27 +0200

    Update vite.config.js
    add support css

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+      external: ['styles.css']
+    }
+  },
   define: {
     'process.env': process.env
   }
